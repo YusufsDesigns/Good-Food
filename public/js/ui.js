78e75a00a48c37fe33d5f9e741ecd0df0b71ed79
Yusuf Lawal <yusuf.olaoluwalawal@outlook.com>
2022-09-30 21:50:09 +0100

    New Commit

--- public/js/ui.js
+++ public/js/ui.js
@@ -8,12 +8,20 @@ class UI{
         let output = '';
         data.forEach(hit => {
             output += `
-                <div class="collection-item shadow-xl">
-                    <div class="">
-                        <img src="${hit.recipe.image}" alt="" class="w-full h-40 object-cover">
+                <div class="collection-item shadow-xl relative cursor-pointer">
+                    <div class="overflow-hidden">
+                        <img src="${hit.recipe.image}" alt="" class="image w-full h-40 object-cover transition-all ">
                     </div>
-                    <div class="p-4 bg-white">
-                        ${hit.recipe.label}
+                    <div class="px-4 py-2 bg-white">
+                    <div class="flex items-center justify-between">
+                        <span class="font-bold">${hit.recipe.label}</span>
+                        <div class="text-yellow-900 text-sm">${hit.recipe.calories.toFixed(0)} cal
+                        </div>
+                    </div>
+                        <div class="flex items-center justify-between my-2">
+                            <span class="text-xs uppercase">Dish Type: ${hit.recipe.dishType}</span>
+                            <span class="text-xs uppercase">Meal Type: ${hit.recipe.mealType}</span>
+                        </div>
                     </div>
                 </div>
             `
